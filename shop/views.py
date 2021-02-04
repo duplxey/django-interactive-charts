ba9f502fa59a90f0fb393c35b51570bb1dbd6c31
duplxey <nick.tomazic@gmail.com>
2021-02-04 20:23:12 +0100

    Urls & views.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_sales_chart(request, year):
+    return JsonResponse({
+        'labels': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'backgroundColor': ['#79AEC8'],
+        'borderColor': ['#79AEC8'],
+        'data': [1, 2, 3, 4, 5],
+    })
+
+
+def spend_per_customer_chart(request, year):
+    return JsonResponse({
+        'labels': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'backgroundColor': ['#79AEC8'],
+        'borderColor': ['#79AEC8'],
+        'data': [1, 2, 3, 4, 5],
+    })
+
+
+def payment_success_chart(request, year):
+
+
+
+    return JsonResponse({
+        'labels': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'backgroundColor': ['#79AEC8'],
+        'borderColor': ['#79AEC8'],
+        'data': [1, 2, 3, 4, 5],
+    })
+
+
+def payment_method_chart(request, year):
+    return JsonResponse({
+        'labels': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'backgroundColor': ['#79AEC8'],
+        'borderColor': ['#79AEC8'],
+        'data': [1, 2, 3, 4, 5],
+    })
