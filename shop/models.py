ef06b3b9181d767024653319a03b9e39ef856991
duplxey <nick.tomazic@gmail.com>
2021-02-05 01:24:00 +0100

    Improved payment_success_chart & payment_method_chart.

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True)
     price = models.FloatField(default=0)
 
     def __str__(self):
@@ -18,6 +18,7 @@ class Purchase(models.Model):
     PAYMENT_METHODS = [
         ('CC', 'Credit card'),
         ('DC', 'Debit card'),
+        ('ET', 'Ethereum'),
         ('BC', 'Bitcoin'),
     ]
     payment_method = models.CharField(max_length=2, default='CC', choices=PAYMENT_METHODS)
--- shop/views.py
+++ shop/views.py
@@ -2,13 +2,16 @@ import random
 from datetime import datetime, timedelta
 
 import pytz
+from django.db.models import Count
 from django.http import JsonResponse
 from django.utils import timezone
 
 from shop.models import Purchase, Item
 
-colorPalette = ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe",
-                "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8"]
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+
+colorPalette = ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8"]
 colorSuccess = colorPalette[0]
 colorDanger = colorSuccess[6]
 
@@ -18,7 +21,6 @@ def generate_color_palette(amount):
     i = 0
     while i < len(colorPalette) and len(palette) < amount:
         palette.append(colorPalette[i])
-        print(colorPalette[i])
         i += 1
         if i == len(colorPalette) and len(palette) < amount:
             i = 0
@@ -26,6 +28,17 @@ def generate_color_palette(amount):
 
 
 def get_sales_chart(request, year):
+    purchases = Purchase.objects.filter(time__year=year)
+
+    # print(
+    #     purchases
+    #         .annotate(price=F('item__price'))
+    #         .annotate(month=TruncMonth('time'))  # Truncate to month and add to select list
+    #         .values('month')                          # Group By month
+    #         .annotate(c=Count('id'))                  # Select the count of the grouping
+    #         .values('month', 'c', 'item__price', 'quantity').order_by()
+    # )
+
     return JsonResponse({
         'labels': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         'backgroundColor': ['#79AEC8'],
@@ -45,33 +58,31 @@ def spend_per_customer_chart(request, year):
 
 def payment_success_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
-    successful, unsuccessful = 0, 0
-
-    for purchase in purchases:
-        if purchase.successful:
-            successful += 1
-        else:
-            unsuccessful += 1
 
     return JsonResponse({
         'labels': ['Successful', 'Unsuccessful'],
         'backgroundColor': [colorSuccess, colorSuccess],
         'borderColor': [colorDanger, colorDanger],
-        'data': [successful, unsuccessful],
+        'data': [
+            purchases.filter(successful=True).count(),
+            purchases.filter(successful=False).count(),
+        ],
     })
 
 
 def payment_method_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
+    grouped_purchases = purchases.values('payment_method').annotate(count=Count('id'))\
+        .values('payment_method', 'count').order_by('payment_method')
+
     payment_method_dict = dict()
 
-    for payment_method in Purchase.PAYMENT_METHODS:
-        payment_method_dict[payment_method[1]] = 0
+    for group in grouped_purchases:
+        payment_method_dict[dict(Purchase.PAYMENT_METHODS)[group['payment_method']]] = group['count']
 
-    for purchase in purchases:
-        payment_type = dict(Purchase.PAYMENT_METHODS)[purchase.payment_method]
-        if payment_type in payment_method_dict:
-            payment_method_dict[payment_type] = payment_method_dict[payment_type] + 1
+    for payment_method in Purchase.PAYMENT_METHODS:
+        if payment_method[1] not in payment_method_dict:
+            payment_method_dict[payment_method[1]] = 0
 
     return JsonResponse({
         'labels': list(payment_method_dict.keys()),
@@ -87,11 +98,15 @@ def generate(request):
 
     names = ["Jack", "John", "Mike", "Chris", "Kyle"]
     surname = ["Jackson", "Smith", "Tyson", "Musk", "Gates"]
-    items = [Item.objects.get(pk=1), Item.objects.get(pk=2), Item.objects.get(pk=3),
-             Item.objects.get(pk=4), Item.objects.get(pk=5)]
+    items = [
+        Item.objects.create(name="Socks", price=6.5), Item.objects.create(name="Pants", price=12),
+        Item.objects.create(name="T-Shirt", price=8), Item.objects.create(name="Boots", price=9),
+        Item.objects.create(name="Sweater", price=3), Item.objects.create(name="Underwear", price=9),
+        Item.objects.create(name="Cap", price=5), Item.objects.create(name="Leggings", price=7),
+    ]
 
     for i in range(0, 2500):
-        dt = pytz.utc.localize(datetime.now() - timedelta(days=random.randint(0, 5)*365))
+        dt = pytz.utc.localize(datetime.now() - timedelta(days=random.randint(0, 1825)))
         purchase = Purchase.objects.create(
             customer_full_name=names[random.randint(0, len(names)-1)] + " " + surname[random.randint(0, len(surname)-1)],
             item=items[random.randint(0, 2)],
