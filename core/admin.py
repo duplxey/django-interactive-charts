3fec45f351db8434ada9e0ed74057e723eecbe4c
Michael Herman <hermanmu@gmail.com>
2023-04-15 11:15:02 -0500

    updates

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,3 @@
-# core/admin.py
-
 from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
@@ -8,8 +6,8 @@ from django.urls import path
 
 @staff_member_required
 def admin_statistics_view(request):
-    return render(request, 'admin/statistics.html', {
-        'title': 'Statistics'
+    return render(request, "admin/statistics.html", {
+        "title": "Statistics"
     })
 
 
@@ -18,14 +16,14 @@ class CustomAdminSite(admin.AdminSite):
         app_list = super().get_app_list(request)
         app_list += [
             {
-                'name': 'My Custom App',
-                'app_label': 'my_custom_app',
-                'models': [
+                "name": "My Custom App",
+                "app_label": "my_custom_app",
+                "models": [
                     {
-                        'name': 'Statistics',
-                        'object_name': 'statistics',
-                        'admin_url': '/admin/statistics',
-                        'view_only': True,
+                        "name": "Statistics",
+                        "object_name": "statistics",
+                        "admin_url": "/admin/statistics",
+                        "view_only": True,
                     }
                 ],
             }
@@ -35,6 +33,6 @@ class CustomAdminSite(admin.AdminSite):
     def get_urls(self):
         urls = super().get_urls()
         urls += [
-            path('statistics/', admin_statistics_view, name='admin-statistics'),
+            path("statistics/", admin_statistics_view, name="admin-statistics"),
         ]
         return urls
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,3 @@
-# shop/views.py
-
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Count, F, Sum, Avg
 from django.db.models.functions import ExtractYear, ExtractMonth
@@ -12,34 +10,34 @@ from utils.charts import months, colorPrimary, colorSuccess, colorDanger, genera
 
 @staff_member_required
 def get_filter_options(request):
-    grouped_purchases = Purchase.objects.annotate(year=ExtractYear('time')).values('year').order_by('-year').distinct()
-    options = [purchase['year'] for purchase in grouped_purchases]
+    grouped_purchases = Purchase.objects.annotate(year=ExtractYear("time")).values("year").order_by("-year").distinct()
+    options = [purchase["year"] for purchase in grouped_purchases]
 
     return JsonResponse({
-        'options': options,
+        "options": options,
     })
 
 
 @staff_member_required
 def get_sales_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
-    grouped_purchases = purchases.annotate(price=F('item__price')).annotate(month=ExtractMonth('time'))\
-        .values('month').annotate(average=Sum('item__price')).values('month', 'average').order_by('month')
+    grouped_purchases = purchases.annotate(price=F("item__price")).annotate(month=ExtractMonth("time"))\
+        .values("month").annotate(average=Sum("item__price")).values("month", "average").order_by("month")
 
     sales_dict = get_year_dict()
 
     for group in grouped_purchases:
-        sales_dict[months[group['month']-1]] = round(group['average'], 2)
+        sales_dict[months[group["month"]-1]] = round(group["average"], 2)
 
     return JsonResponse({
-        'title': f'Sales in {year}',
-        'data': {
-            'labels': list(sales_dict.keys()),
-            'datasets': [{
-                'label': 'Amount ($)',
-                'backgroundColor': colorPrimary,
-                'borderColor': colorPrimary,
-                'data': list(sales_dict.values()),
+        "title": f"Sales in {year}",
+        "data": {
+            "labels": list(sales_dict.keys()),
+            "datasets": [{
+                "label": "Amount ($)",
+                "backgroundColor": colorPrimary,
+                "borderColor": colorPrimary,
+                "data": list(sales_dict.values()),
             }]
         },
     })
@@ -48,23 +46,23 @@ def get_sales_chart(request, year):
 @staff_member_required
 def spend_per_customer_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
-    grouped_purchases = purchases.annotate(price=F('item__price')).annotate(month=ExtractMonth('time'))\
-        .values('month').annotate(average=Avg('item__price')).values('month', 'average').order_by('month')
+    grouped_purchases = purchases.annotate(price=F("item__price")).annotate(month=ExtractMonth("time"))\
+        .values("month").annotate(average=Avg("item__price")).values("month", "average").order_by("month")
 
     spend_per_customer_dict = get_year_dict()
 
     for group in grouped_purchases:
-        spend_per_customer_dict[months[group['month']-1]] = round(group['average'], 2)
+        spend_per_customer_dict[months[group["month"]-1]] = round(group["average"], 2)
 
     return JsonResponse({
-        'title': f'Spend per customer in {year}',
-        'data': {
-            'labels': list(spend_per_customer_dict.keys()),
-            'datasets': [{
-                'label': 'Amount ($)',
-                'backgroundColor': colorPrimary,
-                'borderColor': colorPrimary,
-                'data': list(spend_per_customer_dict.values()),
+        "title": f"Spend per customer in {year}",
+        "data": {
+            "labels": list(spend_per_customer_dict.keys()),
+            "datasets": [{
+                "label": "Amount ($)",
+                "backgroundColor": colorPrimary,
+                "borderColor": colorPrimary,
+                "data": list(spend_per_customer_dict.values()),
             }]
         },
     })
@@ -75,14 +73,14 @@ def payment_success_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
 
     return JsonResponse({
-        'title': f'Payment success rate in {year}',
-        'data': {
-            'labels': ['Successful', 'Unsuccessful'],
-            'datasets': [{
-                'label': 'Amount ($)',
-                'backgroundColor': [colorSuccess, colorDanger],
-                'borderColor': [colorSuccess, colorDanger],
-                'data': [
+        "title": f"Payment success rate in {year}",
+        "data": {
+            "labels": ["Successful", "Unsuccessful"],
+            "datasets": [{
+                "label": "Amount ($)",
+                "backgroundColor": [colorSuccess, colorDanger],
+                "borderColor": [colorSuccess, colorDanger],
+                "data": [
                     purchases.filter(successful=True).count(),
                     purchases.filter(successful=False).count(),
                 ],
@@ -94,8 +92,8 @@ def payment_success_chart(request, year):
 @staff_member_required
 def payment_method_chart(request, year):
     purchases = Purchase.objects.filter(time__year=year)
-    grouped_purchases = purchases.values('payment_method').annotate(count=Count('id'))\
-        .values('payment_method', 'count').order_by('payment_method')
+    grouped_purchases = purchases.values("payment_method").annotate(count=Count("id"))\
+        .values("payment_method", "count").order_by("payment_method")
 
     payment_method_dict = dict()
 
@@ -103,17 +101,17 @@ def payment_method_chart(request, year):
         payment_method_dict[payment_method[1]] = 0
 
     for group in grouped_purchases:
-        payment_method_dict[dict(Purchase.PAYMENT_METHODS)[group['payment_method']]] = group['count']
+        payment_method_dict[dict(Purchase.PAYMENT_METHODS)[group["payment_method"]]] = group["count"]
 
     return JsonResponse({
-        'title': f'Payment method rate in {year}',
-        'data': {
-            'labels': list(payment_method_dict.keys()),
-            'datasets': [{
-                'label': 'Amount ($)',
-                'backgroundColor': generate_color_palette(len(payment_method_dict)),
-                'borderColor': generate_color_palette(len(payment_method_dict)),
-                'data': list(payment_method_dict.values()),
+        "title": f"Payment method rate in {year}",
+        "data": {
+            "labels": list(payment_method_dict.keys()),
+            "datasets": [{
+                "label": "Amount ($)",
+                "backgroundColor": generate_color_palette(len(payment_method_dict)),
+                "borderColor": generate_color_palette(len(payment_method_dict)),
+                "data": list(payment_method_dict.values()),
             }]
         },
     })
@@ -121,4 +119,4 @@ def payment_method_chart(request, year):
 
 @staff_member_required
 def statistics_view(request):
-    return render(request, 'statistics.html', {})
+    return render(request, "statistics.html", {})
